a8b6052b5df99feb415c9b0431423d10a9c4179b
bpmanzo <bpmanzo@student.apc.edu.ph>
2021-01-13 00:22:08 +0800

    Happy Cones Initial Setup(Laravel)

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    //
+    public function index(){
+        // $products = [0=> ["name"=>"Iphone","category"=>"smart phones", "price"=>1000],
+        //         1=> ["name"=>"Galaxy","category"=>"tablets", "price"=>2000],
+        //         2=> ["name"=>"Sony","category"=>"TV", "price"=>3000]];
+        
+        $products = Product::all();
+        
+        return view("allproducts", compact("products"));
+
+    }
+}
